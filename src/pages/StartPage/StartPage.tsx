cca57061596b991e512128a287246cea928140be
Nicolas Mattone <mattonenicolas@gmail.com>
2024-03-16 18:50:02 -0300

    Update start page look and feel

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,3 +1,8 @@
+import './StartPage.css';
+
+import loadingImg from "./../../assets/football.gif";
+import bannerImg from "./../../assets/champions.jpg";
+
 import { useUserContext } from "../../context/UserContext";
 import LoginForm from "./components/LoginForm";
 import SignUpForm from "./components/SignUpForm";
@@ -5,10 +10,13 @@ import { Navigate } from "react-router-dom";
 import useAxios from "../../hooks/useAxios";
 import Alert from "../../components/Shared/Alert/Alert";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 
 const StartPage = () => {
     const { principal } = useUserContext();
+
+    const isDesktop = useMediaQuery('(min-width: 960px)');
 
     const { loading, error } = useAxios({
         lazy: false,
@@ -21,23 +29,29 @@ const StartPage = () => {
         <>
             {loading ? (
                 <>
-                    <Alert type="default">
-                        <FontAwesomeIcon icon="spinner" spin /> Verificando disponibilidad del servidor.
-                        <hr />
-                        <p>
-                            Esto puede tardar varios minutos, por favor, sea paciente y espere.
-                        </p>
-                    </Alert>
+                    <div id="start-page-loading-gif" className="flex-container flex-skeleton-a">
+                        <div className="half-width">
+                            <img src={loadingImg} />
+                        </div>
+                        <div className="half-width text-center">
+                            Comprobando disponibilidad del servidor
+                            <p>
+                                <small>
+                                    Puede tardar entre 5 y 10 minutos. Es un servicio gratuito. No hay plata, es lo que hay. Asique no te quejes.
+                                </small>
+                            </p>
+                        </div>
+                    </div>
                 </>
             ) : (<>
                 {error ? (
                     <>
                         <Alert type="diablo">
                             <FontAwesomeIcon icon="triangle-exclamation" /> <strong>Harry, tenemos un problema!</strong>
-                            <hr/>
+                            <hr />
                             <p>
                                 El servidor no esta disponible. Intente en otro momento.
-                                <br/>
+                                <br />
                                 <small>Si el problema persiste, notifique al administrador.</small>
                             </p>
                         </Alert>
@@ -48,8 +62,24 @@ const StartPage = () => {
                             <Navigate to="/home" replace={true} />
                         ) : (
                             <>
-                                <LoginForm />
-                                <SignUpForm />
+                                {isDesktop ? (
+                                    <>
+                                        <div id="start-page-desktop" className="flex-container">
+                                            <div>
+                                                <img src={bannerImg} />
+                                            </div>
+                                            <div>
+                                                <LoginForm />
+                                                <SignUpForm />
+                                            </div>
+                                        </div>
+                                    </>
+                                ) : (
+                                    <>
+                                        <LoginForm />
+                                        <SignUpForm />
+                                    </>
+                                )}
                             </>
                         )}
                     </>
