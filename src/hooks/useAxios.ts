4aff439fc9ba383a8b43b23af72d86fe9e9e8b7f
Nicolas Mattone <mattonenicolas@gmail.com>
2024-03-16 15:02:49 -0300

    Redirect to login form if session expired

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios, { AxiosError, AxiosResponse } from 'axios';
-import { useUserContext } from '../context/UserContext';
+import { Principal, useUserContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
+import toast from "react-hot-toast";
 
 interface IAxiosRequest {
     lazy: boolean;
@@ -10,7 +12,8 @@ interface IAxiosRequest {
 }
 
 const useAxios = (axiosParams: IAxiosRequest) => {
-    const { principal } = useUserContext();
+    const { principal, setPrincipal } = useUserContext();
+    const navigate = useNavigate();
     const [response, setResponse] = useState<AxiosResponse>();
     const [error, setError] = useState<AxiosError>();
     const [loading, setLoading] = useState(!axiosParams.lazy);
@@ -26,7 +29,25 @@ const useAxios = (axiosParams: IAxiosRequest) => {
             });
             setResponse(result);
         } catch (err) {
-            setError(err as AxiosError);
+            const error = err as AxiosError;
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                if(error.response.status === 401 && !params.url.includes("/auth/")) {
+                    toast("Su sesión ha expirado", { id: "401", icon: "ℹ️" });
+                    setPrincipal(new Principal());
+                    navigate("/");
+                }
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                // console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                // console.log('Error', error.message);
+            }
+            setError(error);
         } finally {
             setLoading(false);
         }
@@ -41,6 +62,7 @@ const useAxios = (axiosParams: IAxiosRequest) => {
         if (!axiosParams.lazy) {
             fetchData(axiosParams);
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return { response, error, loading, sendData };
